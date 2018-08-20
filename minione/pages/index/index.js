19545a5e8fb60ec5e26426c1c30bebe18843e178
togetup <togetup@yeah.net>
2018-08-20 10:58:38 +0800

    获取用户信息

--- minione/pages/index/index.js
+++ minione/pages/index/index.js
@@ -5,7 +5,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-    name:'普惠大众'
+    name:'普惠大众',
+    userInfo:{},
+    isShow:true
   },
 
   handleParent(){
@@ -21,6 +23,54 @@ Page({
    */
   onLoad: function (options) {
     // 做一些初始化工作，发送请求，开启定时器
+
+    this.getUserInfo();
+  },
+
+  /**
+   * 获取用户信息
+   */
+  getUserInfo(){
+    // 判断用户是否已授权
+    wx.getSetting({
+      success: (data) => {
+        if (data.authSetting['scope.userInfo']) {
+          // 用户已授权
+          this.setData({
+            isShow: false
+          })
+        } else {
+          // 用户未授权
+          this.setData({
+            isShow: true
+          })
+        }
+      }
+    })
+
+    // 获取用户登录信息
+    wx.getUserInfo({
+      success: (data) => {
+        // 更新 data 中的 userInfo
+        this.setData({
+          userInfo: data.userInfo
+        })
+      },
+      fail: () => {
+        console.log('获取用户信息失败');
+      }
+    })
+  },
+
+  /**
+   * 处理用户登录
+   */
+  handleGetUserInfo:function(data){
+    // 判断用户点击的是否是允许
+    if(data.detail.rawData){
+      // 用户点击的是允许
+      this.getUserInfo();
+    }
   },
 
   /**
