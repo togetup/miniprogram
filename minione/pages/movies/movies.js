274379bed49f4dd38c1698a8c2846bacacd2415f
togetup <togetup@yeah.net>
2018-08-26 21:28:51 +0800

    电影频道列表显示
    使用request发送请求，电影列表的显示样式可以直接借鉴

--- minione/pages/movies/movies.js
+++ minione/pages/movies/movies.js
@@ -1,25 +1,36 @@
 // pages/movies/movies.js
+const MOVIE_URL = 'http://t.yushu.im/v2/movie/top250';
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    moviesArr:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    wx.request({
+      url: MOVIE_URL,
+      success:(data) => {
+        
+        // 更新状态值
+        this.setData({
+          moviesArr:data.data.subjects
+        });
+      }
+    })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    
   },
 
   /**
