7ef43c237b0bd52f0496ee65c9bd27b42bb79c35
togetup <togetup@yeah.net>
2018-11-25 20:29:49 +0800

    喜欢与不喜欢的实现

--- mybook/pages/classic/classic.js
+++ mybook/pages/classic/classic.js
@@ -1,6 +1,8 @@
 import {ClassicModel} from '../../models/classic.js'
+import {LikeModel} from '../../models/like.js'
 
-let classic = new ClassicModel()
+let classicModel = new ClassicModel()
+let likeModel = new LikeModel()
 
 Page({
 
@@ -15,7 +17,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    classic.getLatest((res)=>{
+    classicModel.getLatest((res) => {
       this.setData({
         classic:res
       })
@@ -23,7 +25,8 @@ Page({
   },
 
 onLike:function(event){
-  console.log(event);
+  let behavior = event.detail.behavior;
+  likeModel.like(behavior, this.data.classic.id, this.data.classic.type);
 },
 
   /**
